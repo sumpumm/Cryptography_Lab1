bc2f766dc32fdeaa59ae8de68e3c66e2a1aabef2
sumpumm <119101433+sumpumm@users.noreply.github.com>
2024-05-25 21:04:22 +0545

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -12,8 +12,8 @@ def encypt_func(txt, s):
             result += chr((ord(char) + s - 97) % 26 + 97)
     return result
 # check the above function
-txt = "A"
-s = 1
+txt = "samarpan"
+s = 2
 result=encypt_func(txt, s)
 
 print("Plain txt : " + txt)
@@ -21,13 +21,13 @@ print("Shift pattern : " + str(s))
 print("Cipher: " + result)
 
 def decrypt(cipher_txt,s):
-  decrypt_text= ""
-  for i in range(len(cipher_txt)):
+    decrypt_text= ""
+    for i in range(len(cipher_txt)):
         char = cipher_txt[i]
         if (char.isupper()):
             decrypt_text += chr((ord(char) - s - 65) % 26 + 65)
         else:
             decrypt_text += chr((ord(char) - s - 97) % 26 + 97)
-        return decrypt_text
+    return decrypt_text
 
 print("Decrypted text: " + decrypt(result, s))
